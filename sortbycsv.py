150ff2598c4d0ed9e6f60ae853bafd6e978ae25a
Aaron Duffy <aduffy70@gmail.com>
2010-07-26 14:11:54 -0600

    Corrected usage - it doesn't print to stdout.

--- sortbycsv.py
+++ sortbycsv.py
@@ -2,7 +2,6 @@
 # sortbycsv.py
 # Take a single file of fasta sequences and break them out into files based on a ccsv file.  Each row in the csv file contains a comma separated list of sequence names that should be in a file together.
 # Example: For several cpGenomes we have a fasta file of all the gene sequences in that genome.  We want to have a file for each gene containing the sequence for that gene from each genome, but the names of the genes in these files are not consistent. We create a spreadsheet table with a column for each taxon and a row for each gene.  The cells contain the sequence names.  If we output that file as a csv, and concatenate all the taxon fasta files into a single fasta file, these can be used with this script to create the files by gene.
-# Prints to STDOUT, so redirect to file.
 # Usage: sortbycsv.py <fasta file of all genes for all taxa in any order> <csv file>
 # Aaron M Duffy aduffy70{at}gmail.com
 # June 2010
